b3f5406d03a8869c57e53dd7000743ee8a418855
lalith akash <lalithakash@laliths-MacBook-Air-2.local>
2026-04-18 00:16:19 +0530

    lalith Added feature/UC-17-Built-in-Sorting

--- TrainConsistMgmtTest.java
+++ TrainConsistMgmtTest.java
@@ -1,36 +1,25 @@
 import org.junit.jupiter.api.Test;
-
 import static org.junit.jupiter.api.Assertions.assertArrayEquals;
 
 public class TrainConsistMgmtTest {
 
     @Test
-    public void testBubbleSortWithUnsortedArray() {
-        int[] capacities = {72, 58, 65, 40, 80, 55};
-        int[] expected = {40, 55, 58, 65, 72, 80};
+    public void testSortBogieTypesAlphabetically() {
+        String[] types = {"Sleeper", "Goods", "Passenger", "Engines"};
+        String[] expected = {"Engines", "Goods", "Passenger", "Sleeper"};
 
-        TrainConsistMgmt.bubbleSort(capacities);
+        TrainConsistMgmt.sortBogieTypes(types);
 
-        assertArrayEquals(expected, capacities);
+        assertArrayEquals(expected, types);
     }
 
     @Test
-    public void testBubbleSortWithAlreadySortedArray() {
-        int[] capacities = {10, 20, 30, 40};
-        int[] expected = {10, 20, 30, 40};
+    public void testSortBogieTypesAlreadySorted() {
+        String[] types = {"Box", "Flat", "Tank"};
+        String[] expected = {"Box", "Flat", "Tank"};
 
-        TrainConsistMgmt.bubbleSort(capacities);
+        TrainConsistMgmt.sortBogieTypes(types);
 
-        assertArrayEquals(expected, capacities);
-    }
-
-    @Test
-    public void testBubbleSortWithSingleElement() {
-        int[] capacities = {50};
-        int[] expected = {50};
-
-        TrainConsistMgmt.bubbleSort(capacities);
-
-        assertArrayEquals(expected, capacities);
+        assertArrayEquals(expected, types);
     }
 }
